57f3a077416d9e35cc9c01310e92dedb76f5257b
ferkocaosmnli <ferkocaosmnli@gmail.com>
2025-09-18 14:55:03 +0300

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+let js = "amazing";
+if (js == "amazing");
+
+console.log("merhaba");
+
+let firstName = "ferhat";
+console.log(firstName);
+
+let teacher1 = "tarih";
+let teacher2 = "matematik";
+
+let ogrenci1 = "gülağa";
+let ogrenci2 = "umut";
+
+console.log(teacher1+" "+teacher2+" ogr;"+ogrenci1);
+
+let countr = "turkiye";
+let contient = "europe";
+let population = 85000;
+
+console.log(countr+contient+population);
