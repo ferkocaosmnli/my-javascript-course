91f5b757f2b037350bbaf8260058dda4201c766d
ferkocaosmnli <ferkocaosmnli@gmail.com>
2025-09-19 14:30:30 +0300

    Class üzerinde güncelleme yapıldı

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 if (js == "amazing");
 
@@ -8,14 +9,59 @@ console.log(firstName);
 
 let teacher1 = "tarih";
 let teacher2 = "matematik";
-
+// veri tanımalam
 let ogrenci1 = "gülağa";
 let ogrenci2 = "umut";
-
-console.log(teacher1+" "+teacher2+" ogr;"+ogrenci1);
+let ogrenci3 = "hüso"
+console.log(teacher1+" "+teacher2+" ogr;"+ogrenci1+ogrenci3);
 
 let countr = "turkiye";
 let contient = "europe";
 let population = 85000;
 
 console.log(countr+contient+population);
+
+*/
+
+/*
+true;
+console.log(true);
+let javascript = false;
+console.log(javascript)
+console.log(typeof true)
+
+*/
+
+/*
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 2004;
+console.log(birthYear);
+*/
+
+const now = 2025;
+const ageFerhat = now - 2004;
+const ageNilsu = now - 2009;
+console.log(ageFerhat,ageNilsu);
+console.log(ageFerhat * 2, ageNilsu / 2);
+
+
+const firstName = "ferhat";
+const lastName = "kocaosmanli";
+console.log(firstName+" "+lastName);
+
+// operotoler
+let x = 10+5;
+x+=10; // x = x + 10 = 25
+x *=4; // x = x * 4 = 100
+x++;
+x--;
+console.log(x);
+
+// karşılaştırma
+console.log(ageFerhat < ageNilsu);
+console.log(ageFerhat != ageNilsu);
+
+
